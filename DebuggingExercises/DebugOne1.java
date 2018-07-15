b30e7f83c2070cc72da8c6021a340a19c135db2a
yash590 <yashsanjay.agarwal@my.jcu.edu.au>
2018-07-15 23:18:08 +0800

    correct the errors and change file name

--- DebuggingExercises/DebugOne1.java
+++
@@ -1,7 +0,0 @@
-public class DebugOne1
-
-   /* This program displays a greeting */
-   public static void main(String[] args)
-   {
-      Systemoutprintl("Hello).
-   }
--- DebuggingExercises/DebugOne4.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class DebugOne4
-{
-  public static main(String[] args)
-  {
-     JOptionPane.showMessageDialog(null, First GUI program)!
-  }
-}
